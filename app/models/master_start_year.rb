c89907df200b08025891f4cc94c7fd22841f8f40
sahilchopra94 <sahilchopra94@yahoo.co.in>
2017-04-27 19:37:20 +0530

    Vehicle-start year table

--- app/models/master_start_year.rb
+++ app/models/master_start_year.rb
@@ -1,2 +1,5 @@
 class MasterStartYear < ApplicationRecord
+	has_many :vehicle_start_years
+	has_many :vehicles, through: :vehicle_start_years
+    
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,6 @@
 class Vehicle < ApplicationRecord
-	belongs_to :car
+	has_many :vehicle_start_years
+	has_many :master_start_years, through: :vehicle_start_years
+    belongs_to :car
 	belongs_to :master_variant
 end

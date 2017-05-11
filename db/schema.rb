2e13e33dc9285ecfbb4d8666a65cec2c72fe3f66
sahilchopra94 <sahilchopra94@yahoo.co.in>
2017-05-11 16:14:55 +0530

    Service pages

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,8 @@ ActiveRecord::Schema.define(version: 20170510121751) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "brand_name"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.integer  "user_id",    default: 1
-    t.index ["user_id"], name: "index_brands_on_user_id", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "cars", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -122,7 +120,6 @@ ActiveRecord::Schema.define(version: 20170510121751) do
     t.index ["master_variant_id"], name: "index_vehicles_on_master_variant_id", using: :btree
   end
 
-  add_foreign_key "brands", "users"
   add_foreign_key "users", "roles"
   add_foreign_key "vehicle_start_years", "master_start_years"
   add_foreign_key "vehicle_start_years", "vehicles"
